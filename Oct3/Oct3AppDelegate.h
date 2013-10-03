adcd507a4b1d0f27d40026b12deda4739a97f830
James Neely <jamesneely@James-Neelys-MacBook-Pro.local>
2013-10-02 20:33:44 -0400

    Initial Commit

---
+++ Oct3/Oct3AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct3AppDelegate.h
+//  Oct3
+//
+//  Created by James Neely on 10/2/13.
+//  Copyright (c) 2013 James Neely. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct3AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
